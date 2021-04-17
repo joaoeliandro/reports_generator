4bee6ba68a743a9e3a02b7daa97f20a6b93f66e5
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-17 20:39:58 -0300

    Added first parallel process

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -22,6 +22,12 @@ defmodule ReportsGenerator do
     end)
   end
 
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
@@ -30,26 +36,39 @@ defmodule ReportsGenerator do
 
   defp sum_values(
          [id, food_name, price],
-         %{"foods" => foods, "users" => users} = report
+         %{"foods" => foods, "users" => users}
        ) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
-    update_report(report, users, foods)
+    build_report(foods, users)
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{"foods" => foods2, "users" => users2}) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
 
-  defp update_report(report, users, foods) do
-    # report
-    # |> Map.put("users", users)
-    # |> Map.put("foods", foods)
+  # defp update_report(report, users, foods) do
+  #   # report
+  #   # |> Map.put("users", users)
+  #   # |> Map.put("foods", foods)
 
-    %{report | "users" => users, "foods" => foods}
-  end
+  #   %{report | "users" => users, "foods" => foods}
+  # end
+
+  defp build_report(foods, users), do: %{"users" => users, "foods" => foods}
 end
