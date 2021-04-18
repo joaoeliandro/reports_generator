93a9890b50c120f204dce81d29c795db21c2bd31
João Eliandro Germano <eliandrogermano@gmail.com>
2021-04-17 21:27:36 -0300

    Added tests for parallel function

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -59,6 +59,73 @@ defmodule ReportsGeneratorTest do
     end
   end
 
+  describe "build_from_parallel/1" do
+    test "should be able to build the reports in parallel" do
+      # SETUP
+      file_names = ["report_test.csv", "report_test.csv"]
+
+      # EXERCISE
+      response = ReportsGenerator.build_from_parallel(file_names)
+
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 2,
+          "churrasco" => 4,
+          "esfirra" => 6,
+          "hambúrguer" => 4,
+          "pastel" => 0,
+          "pizza" => 4,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 96,
+          "10" => 72,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 90,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 62,
+          "30" => 0,
+          "4" => 84,
+          "5" => 98,
+          "6" => 36,
+          "7" => 54,
+          "8" => 50,
+          "9" => 48
+        }
+      }
+
+      # ASSERTION
+      assert response == expected_response
+    end
+
+    test "should not be able to build the reports in parallel" do
+      # EXERCISE
+      response = ReportsGenerator.build_from_parallel("error")
+
+      expected_response = {:error, "Please provide a list of strings"}
+      # ASSERTION
+      assert response == expected_response
+    end
+  end
+
   describe "fetch_higher_cost/2" do
     test "should be able to return with option 'users' the user who spent the most" do
       file_name = "report_test.csv"
